c72d999b63d414de28f1eb5e0335ec87fe8918cd
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-09-02 08:36:24 +1000

    wk1

--- wk1.R
+++ wk1.R
@@ -18,3 +18,25 @@ myHist <- function(mu){
     text(63, 140, paste("MSE = ", round(mse, 2)))
 }
 manipulate(myHist(mu), mu = slider(62, 74, step = 0.5))
+
+png('least_sqr.png')
+hist(galton$child,col="blue",breaks=100)
+meanChild <- mean(galton$child)
+lines(rep(meanChild,100),seq(0,150,length=100),col="red",lwd=5)
+dev.off()
+
+# galton
+png('galton.png')
+plot(galton$parent, galton$child, pch=19, col='blue')
+dev.off()
+
+png('galton_freq.png')
+freqData <- as.data.frame(table(galton$child, galton$parent))
+names(freqData) <- c("child", "parent", "freq")
+plot(as.numeric(as.vector(freqData$parent)), 
+     as.numeric(as.vector(freqData$child)),
+     pch = 21, col = "black", bg = "lightblue",
+     cex = .15 * freqData$freq, 
+     xlab = "parent", ylab = "child")
+dev.off()
+
