185fb3fb3101715ca53fa6646fa45b7bcfc1e6c5
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-09-02 08:42:31 +1000

    wk1

--- wk1.R
+++ wk1.R
@@ -40,3 +40,47 @@ plot(as.numeric(as.vector(freqData$parent)),
      xlab = "parent", ylab = "child")
 dev.off()
 
+myPlot <- function(beta){
+    y <- galton$child - mean(galton$child)
+    x <- galton$parent - mean(galton$parent)
+    freqData <- as.data.frame(table(x, y))
+    names(freqData) <- c("child", "parent", "freq")
+    plot(
+        as.numeric(as.vector(freqData$parent)), 
+        as.numeric(as.vector(freqData$child)),
+        pch = 21, col = "black", bg = "lightblue",
+        cex = .15 * freqData$freq, 
+        xlab = "parent", 
+        ylab = "child"
+    )
+    abline(0, beta, lwd = 3)
+    points(0, 0, cex = 2, pch = 19)
+    mse <- mean( (y - beta * x)^2 )
+    title(paste("beta = ", beta, "mse = ", round(mse, 3)))
+}
+png('beta.png')
+manipulate(myPlot(beta), beta = slider(0.6, 1.2, step = 0.02))
+dev.off()
+
+# solution
+lm(I(child-mean(child))~I(parent-mean(parent))-1,data=galton)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
