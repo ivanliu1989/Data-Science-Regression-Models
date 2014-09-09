2c3ef5a68cf31f7591efb8c02a0398249ba8d811
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-09-09 20:48:49 +1000

    wk2

--- wk2_residual_var.R
+++ wk2_residual_var.R
@@ -7,3 +7,15 @@ e <- resid(fit)
 yhat <- predict(fit)
 max(abs(e -(y - yhat)))
 max(abs(e - (y - coef(fit)[1] - coef(fit)[2] * x)))
+
+png('residual1.png')
+plot(diamond$carat, diamond$price,  
+     xlab = "Mass (carats)", 
+     ylab = "Price (SIN $)", 
+     bg = "lightblue", 
+     col = "black", cex = 1.1, pch = 21,frame = FALSE)
+abline(fit, lwd = 2)
+for (i in 1 : n) 
+    lines(c(x[i], x[i]), c(y[i], yhat[i]), col = "red" , lwd = 2)
+dev.off()
+
