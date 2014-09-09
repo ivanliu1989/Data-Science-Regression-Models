c81fee73e5a97a437e670b99bca93cd7d465c2b2
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-09-09 20:30:15 +1000

    wk2

--- wk2_stats_lm.R
+++ wk2_stats_lm.R
@@ -1,1 +1,11 @@
 setwd("C:\\Users\\Ivan.Liuyanfeng\\Desktop\\Data_Mining_Work_Space\\Data-Science-Regression-Models")
+library(UsingR)
+data(diamond)
+png("slm.png")
+plot(diamond$carat, diamond$price,
+     xlab = "Mass (carats)",
+     ylab = "Price (SIN $)",
+     bg = "lightblue",
+     col = "black", cex = 1.1, pch = 21,frame = FALSE)
+abline(lm(price ~ carat, data = diamond), lwd = 2)
+dev.off()
