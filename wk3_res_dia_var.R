a1f8257c348caa0fd8818a1244be1ddd9e153d61
ivan-liuyanfeng <ivan.liuyanfeng@gmail.com>
2014-09-14 11:17:15 +1000

    wk3

--- wk3_res_dia_var.R
+++ wk3_res_dia_var.R
@@ -15,3 +15,31 @@ points(0, 5, cex = 2, bg = "darkorange", col = "black", pch = 21)
 points(5, 5, cex = 2, bg = "darkorange", col = "black", pch = 21)
 points(5, 0, cex = 2, bg = "darkorange", col = "black", pch = 21)
 dev.off()
+
+?influence.measures
+
+n <- 100; x <- c(10, rnorm(n)); y <- c(10, c(rnorm(n)))
+plot(x, y, frame = FALSE, cex = 2, pch = 21, bg = "lightblue", col = "black")
+abline(lm(y ~ x))
+fit <- lm(y ~ x)
+round(dfbetas(fit)[1 : 10, 2], 3)
+round(hatvalues(fit)[1 : 10], 3)
+
+x <- rnorm(n); y <- x + rnorm(n, sd = .3)
+x <- c(5, x); y <- c(5, y)
+plot(x, y, frame = FALSE, cex = 2, pch = 21, bg = "lightblue", col = "black")
+fit2 <- lm(y ~ x)
+abline(fit2)      
+round(dfbetas(fit2)[1 : 10, 2], 3)
+round(hatvalues(fit2)[1 : 10], 3)
+
+## Don't everyone hit this server at once. Read the paper first.
+dat <- read.table('http://www4.stat.ncsu.edu/~stefanski/NSF_Supported/Hidden_Images/orly_owl_files/orly_owl_Lin_4p_5_flat.txt', header = FALSE)
+pairs(dat)
+summary(lm(V1 ~ . -1, data = dat))$coef
+png("wk3_residual.png")
+fit <- lm(V1 ~ . - 1, data = dat); plot(predict(fit), resid(fit), pch = '.')
+dev.off()
+
+data(swiss); par(mfrow = c(1, 1))
+fit <- lm(Fertility ~ . , data = swiss); plot(fit)
