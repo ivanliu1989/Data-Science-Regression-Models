0c5aba0f453449e6ba44a162b94e9439e93471a6
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-09-17 14:42:48 +1000

    quiz4

--- quiz4.R
+++ quiz4.R
@@ -1,1 +1,12 @@
 setwd('/Users/ivan/Work_directory/Data-Science-Regression-Models')
+Consider the space shuttle data ?shuttle in the MASS library. 
+Consider modeling the use of the autolander as the outcome (variable name use). 
+Fit a logistic regression model with autoloader (variable auto) use (labeled as "auto" 1) 
+versus not (0) as predicted by wind sign (variable wind). 
+Give the estimated odds ratio for autoloader use comparing head winds, 
+labeled as "head" in the variable headwind (numerator) to tail winds (denominator).
+
+require(MASS)
+data(shuttle)
+str(shuttle)
+
